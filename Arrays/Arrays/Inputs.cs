f09a3151c73bb6703ced338bcc0e9b0b28b85c93
Miklos Rago <rago.miklos@gmail.com>
2018-10-10 17:18:02 +0200

    Kinda bad implementation of a array input selection

---
+++ Arrays/Arrays/Inputs.cs
@@ -0,0 +1,25 @@
+﻿namespace Arrays
+{
+    class Inputs
+    {
+        protected int _number;
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        protected string _color;
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public Inputs(int number, string color)
+        {
+            Number = number;
+            Color = color;
+        }
+    }
+}
