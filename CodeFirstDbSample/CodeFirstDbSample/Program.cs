f28271b763e5c180eb9ab42e57d2249e03838dee
Miklos Rago <rago.miklos@gmail.com>
2018-10-15 18:58:44 +0200

    Code first implementation of EF usage

---
+++ CodeFirstDbSample/CodeFirstDbSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using CodeFirstDbSample.Model;
+using System;
+using System.Linq;
+
+namespace CodeFirstDbSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BlogContext())
+            {
+                Console.Write("Enter a name for a new blog:");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+        }
+    }
+}
