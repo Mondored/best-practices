fa3293d1b1901a18baa79cc3a1aa100455f59a05
Miklos Rago <rago.miklos@gmail.com>
2018-10-24 22:05:02 +0200

    SimpleClass demo for calculate raise for an employee

---
+++ SimpleClass_Demo/SimpleClass_Demo/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleClass_Demo
+{
+    public class Employee
+    {
+        public enum Rating
+        {
+            poor,
+            good,
+            excellent
+        }
+
+        private Rating rating;
+        public double Income { get; set; }
+        public int YearsOfService { get; set; }
+
+        public Employee(double income, int yearsOfService)
+        {
+            Income = income;
+            YearsOfService = yearsOfService;
+        }
+
+        public Employee()
+        {
+        }
+
+        public void SetRating(Rating rating)
+        {
+            this.rating = rating;
+        }
+
+        public void CalculateRaise()
+        {
+            double baseRaise = Income * .05;
+            double bonus = YearsOfService * 1000;
+
+            Income += baseRaise + bonus;
+
+            switch (rating)
+            {
+                case Rating.poor:
+                    Income -= YearsOfService * 2000;
+                    break;
+                case Rating.good:
+                    break;
+                case Rating.excellent:
+                    Income += YearsOfService * 500;
+                    break;
+            }
+
+            Console.WriteLine($"New Income: {Income}");
+        }
+    }
+}
---
+++ SimpleClass_Demo/SimpleClass_Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimpleClass_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee tony = new Employee(150000,8);
+            tony.Income = 150000;
+            tony.YearsOfService = 8;
+            tony.SetRating(Employee.Rating.excellent);
+            tony.CalculateRaise();
+
+            Employee sam = new Employee()
+            {
+                Income = 145000,
+                YearsOfService = 3
+            };
+
+            sam.SetRating(Employee.Rating.good);
+            sam.CalculateRaise();
+
+            Console.ReadKey();
+        }
+    }
+}
