587d93b7bdbe4f5be812fd836c735329e9599171
Miklos Rago <rago.miklos@gmail.com>
2018-11-23 20:39:05 +0100

    Using Linq expressions in different inputs

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+
+            FourOther fourOther = new FourOther();
+
+            Console.ReadKey();
+        }
+    }
+}
