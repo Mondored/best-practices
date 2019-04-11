7699bf102d1cbd26e21b7933d9533ec3af9bf190
Miklos Rago <rago.miklos@gmail.com>
2019-04-11 18:05:50 +0200

    Registering authRepository service in the Startup class

--- DatingApp/src/DatingApp.API/Startup.cs
+++ DatingApp/src/DatingApp.API/Startup.cs
@@ -31,6 +31,10 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             //To mitigate the CORS issue between the two component
             services.AddCors();
+            //Used different reasons the Singleton, Transient or AddScoped
+            //services.AddSingleton<IAuthRepository, AuthRepository>();
+            //services.AddTransient<IAuthRepository, AuthRepository>();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
