344e7f3e5094378d85eaaf0e529d8d325d46486f
Miklos Rago <rago.miklos@gmail.com>
2019-04-15 15:30:26 +0200

    Adding Authorization controller and dataTransfareObject for registration

---
+++ DatingApp/src/DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            //validate request
+            userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower();
+            
+            if(await _repo.UserExists(userForRegisterDto.UserName))
+            {
+                return BadRequest("Username already exists");
+            }
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.UserName
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.PassWord);
+
+            return StatusCode(201);
+        }
+    }
+}
---
+++ DatingApp/src/DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+    }
+}
