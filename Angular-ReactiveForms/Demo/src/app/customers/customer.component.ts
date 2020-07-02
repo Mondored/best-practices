4238615e9a6f178da0488a4987ef4042706e0fdf
Ragó Miklós <rago.miklos@gmail.com>
2020-07-02 15:59:36 +0200

    Switch from template driven approach to create reactive forms

--- Angular-ReactiveForms/Demo/src/app/customers/customer.component.ts
+++ Angular-ReactiveForms/Demo/src/app/customers/customer.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
@@ -9,15 +8,29 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit() {
+    this.customerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: {value: 'n/a', disabled: true},
+      email: '',
+      sendCatalog: true
+    });
+
+    /* this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true)      
+    }); */
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 }
