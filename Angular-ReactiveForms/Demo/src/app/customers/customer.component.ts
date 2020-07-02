3613d51c489f3fb326cc436dade38ae9b780b38c
Ragó Miklós <rago.miklos@gmail.com>
2020-07-02 18:31:22 +0200

    Add cross field nested validation

--- Angular-ReactiveForms/Demo/src/app/customers/customer.component.ts
+++ Angular-ReactiveForms/Demo/src/app/customers/customer.component.ts
@@ -27,7 +27,10 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.formBuilder.group({
       firstName: ['', [ Validators.required, Validators.maxLength(3) ] ],
       lastName: ['', [ Validators.required, Validators.maxLength(50) ] ],
-      email: ['', [ Validators.required, Validators.email ] ],
+      emailGroup: this.formBuilder.group({
+        email: ['', [ Validators.required, Validators.email ] ],
+        confirmEmail: ['', [ Validators.required ] ]
+      }),
       phone: '',
       notification: 'email',
       rating: [null, ratingRange(1,5)],
