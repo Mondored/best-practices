d1a76559334ac8f4f59003d6a7e43e0d91f35a18
Ragó Miklós <rago.miklos@gmail.com>
2020-07-02 19:21:44 +0200

    Cleanup and add reactive transformations, debounceTime delay on validation checkings

--- Angular-ReactiveForms/Demo/src/app/customers/customer.component.ts
+++ Angular-ReactiveForms/Demo/src/app/customers/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators'
 
 import { Customer } from './customer';
 
@@ -34,6 +35,12 @@ function ratingRange(min: number, max: number): ValidatorFn {
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
+  emailMessage: string;
+
+  private validationMessages = {
+    required: 'Please enter your email address.',
+    email: 'Please enter a valid email address.'
+  };
 
   constructor(private formBuilder:FormBuilder) { }
 
@@ -57,11 +64,30 @@ export class CustomerComponent implements OnInit {
       email: new FormControl(),
       sendCatalog: new FormControl(true)      
     }); */
+
+    this.customerForm.get('notification').valueChanges.subscribe(
+      value => this.setNotification(value)
+    );
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      value => this.setMessage(emailControl)
+    );
   }
 
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if((c.touched || c.dirty) && c.errors) {
+      this.emailMessage = Object.keys(c.errors).map(
+        key => this.validationMessages[key]).join(' ');
+    }
   }
 
   setNotification(notifyVia: string): void {
