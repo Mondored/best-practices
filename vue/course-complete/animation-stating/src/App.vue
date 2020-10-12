8fdfe88f1984cf91ed630dc2c634a804bc8ea969
Ragó Miklós <rago.miklos@gmail.com>
2020-10-12 22:31:39 +0200

    Add new projects

---
+++ vue/course-complete/animation-stating/src/App.vue
@@ -0,0 +1,148 @@
+<template>
+<div>
+  <div class="container">
+    <user-list></user-list>
+  </div>
+  <div class="container">
+    <div class="block" :class="{animate: animatedBlock}"></div>
+    <button @click="animateBlock">Animate</button>
+  </div>
+  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+    <p>This is a test dialog!</p>
+    <button @click="hideDialog">Close it!</button>
+  </base-modal>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+    <button @click="showUsers" v-if="!usersAreVisible">Show users</button>
+    <button @click="hideUsers" v-else>Hide users</button>
+    </transition>
+  </div>
+  <div class="container">
+    <button @click="showDialog">Show Dialog</button>
+  </div>
+</div>
+</template>
+
+<script>
+import UserList from './components/UserList.vue';
+
+export default {
+  components: {
+    UserList
+  },
+  data() {
+    return {
+      animatedBlock: false,
+      dialogIsVisible: false, 
+      usersAreVisible: false,
+    };
+  },
+  methods: {
+    showUsers(){
+      this.usersAreVisible = true;
+    },
+    hideUsers(){
+      this.usersAreVisible = false;
+    },
+    animateBlock() {
+      this.animatedBlock = true;
+    },
+    showDialog() {
+      this.dialogIsVisible = true;
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+button {
+  font: inherit;
+  padding: 0.5rem 2rem;
+  border: 1px solid #810032;
+  border-radius: 30px;
+  background-color: #810032;
+  color: white;
+  cursor: pointer;
+}
+button:hover,
+button:active {
+  background-color: #a80b48;
+  border-color: #a80b48;
+}
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #290033;
+  margin-bottom: 2rem;
+  /* transition: transform 0.3s ease-out; */
+}
+.container {
+  max-width: 40rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 12px;
+}
+.animate {
+  /* transform: translateX(50px); */
+  animation: slide-scale 0.3s ease-out;
+}
+
+.v-enter-from {
+  opacity: 0;
+  transform:  translateY(-30px);
+}
+
+.v-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.fade-button-enter-from {
+  opacity: 0;
+}
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-button-enter-to {
+  opacity: 1;
+}
+
+/*.v-leave-from {}
+.v-leave-active {}
+.v-leave-to {}*/
+
+@keyframes slide-scale {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1s);
+  }
+}
+</style>
---
+++ vue/course-complete/prj-cmp-starting/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul>
+    <resource-item v-for="res in storedResources"
+     :key="res.id"
+     :title="res.title"
+     :description="res.description"
+     :link="res.link"
+    ></resource-item>
+  </ul>    
+</template>
+
+<script>
+import ResourceItem from './components/learningResources/ResourceItem.vue';
+
+export default {
+  components: {
+    ResourceItem,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        },
+      ],
+    };
+  },
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
