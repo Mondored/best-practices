de9ab12b06a929693de50c6eb824a77252912576
KUKA\rago <miklos.rago@kuka.com>
2020-10-21 14:04:49 +0200

    Environment fixes

---
+++ vue/robotapp/tests/integration/exampleCypress.test.js
@@ -0,0 +1,10 @@
+"use strict";
+describe('TestProject', () => {
+    beforeEach(function () {
+        cy.visit('http://localhost:8080');
+    });
+    it('Do something', () => {
+        expect(true).toBe(true);
+    });
+});
+//# sourceMappingURL=exampleCypress.test.js.map
